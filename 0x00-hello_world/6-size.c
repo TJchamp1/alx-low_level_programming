243cc1f19d8e3eb68b19510fae0af81048f89848
TJchamp1 <tamratabrham9@gmail.com>
2022-06-02 15:22:20 -0500

    hello world task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {printf("Size of a char: 1 byte(s)\n");
-printf("Size of an in: 4 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
 printf("Size of a long int: 4 byte(s)\n");
-printf("Size of along long int: 8 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
 printf("Size of a float: 4 byte(s)");
 return 0;
 }
