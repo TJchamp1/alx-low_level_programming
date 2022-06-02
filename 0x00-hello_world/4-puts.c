df5d48f10dc8063b193d2c9662644169a49a647c
TJchamp1 <tamratabrham9@gmail.com>
2022-06-02 13:58:06 -0500

    hello world task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {  
-puts("\"Programming is like building a multilingual puzzle");/*tired of zis shit*/
+puts("\"Programming is like building a multilingual puzzle");
+/*tired of zis shit*/
 return (0);
 }
