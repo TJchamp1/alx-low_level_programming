b57865cc5acda6b85a937d65e94558b7ef10fba8
TJchamp1 <tamratabrham9@gmail.com>
2022-06-02 15:20:48 -0500

    hello world task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,10 @@
  * Return: 0
  */
 int main(void)
-{
+{printf("Size of a char: 1 byte(s)\n");
+printf("Size of an in: 4 byte(s)\n");
+printf("Size of a long int: 4 byte(s)\n");
+printf("Size of along long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)");
 return 0;
 }
