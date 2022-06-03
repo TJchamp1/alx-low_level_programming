45b3667c65d11abc6dd3f1aa85e82c263045058a
TJchamp1 <tamratabrham9@gmail.com>
2022-06-03 12:41:01 -0500

    variables task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+int n=122, i;
+for (i = 97; i <= n; i++ )
+putchar("%c\n",i);
+return (0);
+}
+
