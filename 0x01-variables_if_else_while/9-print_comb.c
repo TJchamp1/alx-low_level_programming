9f949340c07fa76a12c525b53f2ae41a24bc8a35
TJchamp1 <tamratabrham9@gmail.com>
2022-06-03 15:41:01 -0500

    variables task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,17 @@
  */
 int main(void)
 {
-int i, j = 44;
+int i;
 for (i = 0; i < 10; i++)
 {
-if (i <= 8)
-{
 putchar(i % 10 + '0');
-putchar(j + ' ');
+putchar(',');
+putchar(' ');
 }
-else if (i == 9)
-{
+
 putchar(i % 10 + '0');
-}
-}
+putchar('\n');
+
 return (0);
 }
 
