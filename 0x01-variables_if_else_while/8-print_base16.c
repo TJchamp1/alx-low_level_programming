547001f3a50470719b44e87c5e78f7e476164b87
TJchamp1 <tamratabrham9@gmail.com>
2022-06-03 14:38:14 -0500

    variables task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 int i;
-for (i = 0; i < 16; i++)
+for (i = 0; i < 10; i++)
 putchar(i % 10 + '0');
 
-for(i = 97; i <= 101; i++ )
+for (i = 97; i <= 102; i++)
 putchar(i);
 putchar('\n');
 return (0);
