af12f8e8cb9ee69dcb25b060c658e839a0cf30a2
TJchamp1 <tamratabrham9@gmail.com>
2022-06-03 16:11:28 -0500

    variables task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Print combinations of two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int tow, hunds, tens, ones;
+for (tow = 0; tow <= 9; tow++)
+{
+for (hunds = 0; hunds <= 9; hunds++)
+{
+for (tens = 0; tens <= 9; tens++)
+{
+for (ones = 0; ones <= 9; ones++)
+{
+putchar(tow + '0');
+putchar(hunds + '0');
+putchar(' ');
+putchar(tens + '0');
+putchar(ones + '0');
+if (hunds < 7)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+{
+putchar('\n');
+return (0);
+}
