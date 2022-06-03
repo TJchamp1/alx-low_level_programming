7042bbbf723ffc9c20f3e3aefce887f160032072
TJchamp1 <tamratabrham9@gmail.com>
2022-06-03 14:19:13 -0500

    variables task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+int n = 97, i;
+for (i = 122; i == n; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
