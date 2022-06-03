79830621dd7f1ee0c5e937d9cf6f0e4186b969b7
TJchamp1 <tamratabrham9@gmail.com>
2022-06-03 14:45:31 -0500

    variables task 9

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i <= 8)
+{
+putchar(i % 10 + '0' + ',');
+}
+else if (i == 9)
+{
+putchar(i % 10 + '0');
+}
+}
+putchar('\n');
+return (0);
+}
+
