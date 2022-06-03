a67a447f1aaa4a93ccdc9af1c1b2690d5ae4895d
TJchamp1 <tamratabrham9@gmail.com>
2022-06-03 14:11:37 -0500

    variables task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(atoi(i));
+}
+putchar('\n');
+return (0);
+}
+
