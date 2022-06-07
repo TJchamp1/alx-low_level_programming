a5bcb472d6c9be259cd259be0c4f3421bf7aeb3f
TJchamp1 <tamratabrham9@gmail.com>
2022-06-07 15:56:45 -0500

    function and nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * funtion - funtction block
+ * Return: noting
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+_putchar(\n)
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+_putchar('\n');
+return (0);
+}
+else if (n < 0)
+{
+_putchar('-');
+_putchar('\n');
+return -1;
+}
+}
