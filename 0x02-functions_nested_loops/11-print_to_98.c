03995774ce7446ec0b0e5a468502ff138475e471
TJchamp1 <tamratabrham9@gmail.com>
2022-06-07 17:02:54 -0500

    function and nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * funtion - funtction block
+ * Return: noting
+*/
+void print_to_98(int n)
+{
+int i;
+if (n > 98)
+{
+for (i = n; i < 98; i++)
+{
+_putchar(i + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar(98 + '0');
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+_putchar(i + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar(98 + '0');
+}
+}
+
