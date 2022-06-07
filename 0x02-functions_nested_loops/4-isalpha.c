a529b662742364dc101fc392a7a641eb500b7d9e
TJchamp1 <tamratabrham9@gmail.com>
2022-06-07 15:41:19 -0500

    function and nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * funtion - funtction block
+ * Return: noting
+*/
+int _islower(int c)
+{
+if (c >= 97 && c <= 122 || c >= 65 && c <= 90 || c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
