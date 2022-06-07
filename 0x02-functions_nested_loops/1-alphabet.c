a33ac2c9ba261d85ddcecddd587fda970f517079
TJchamp1 <tamratabrham9@gmail.com>
2022-06-07 06:02:31 -0500

    funtions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<main.h>
+/** 
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+print_alphabet(void)
+{
+int i;
+for (i = 0; i <= 122; i++ )
+{
+putchar(i);
+}
+}
