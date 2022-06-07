d40c274cafef13c05714c01cb3614c4ebdc039f3
TJchamp1 <tamratabrham9@gmail.com>
2022-06-07 15:58:33 -0500

    function and nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-_putchar(\n)
+_putchar('\n');
 return (1);
 }
 else if (n == 0)
@@ -21,6 +21,6 @@ else if (n < 0)
 {
 _putchar('-');
 _putchar('\n');
-return -1;
+return (-1);
 }
 }
