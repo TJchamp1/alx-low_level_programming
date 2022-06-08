fb6c06753791cb89b4bc96ac293e075a109e4912
TJchamp1 <tamratabrham9@gmail.com>
2022-06-08 15:43:28 -0500

    function and nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet - function that prints alphabets
- * @n: integer 
- * Return: @n the value of the last digit
+* print_alphabet - function that prints alphabets
+* adsf
+* Return: @n the value of the last digit
 */
 void  print_alphabet(void)
 {
