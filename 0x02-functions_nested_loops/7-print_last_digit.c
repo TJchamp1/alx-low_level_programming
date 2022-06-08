17744d00ec91a2c4974fe6581dd1ebcc849b0609
TJchamp1 <tamratabrham9@gmail.com>
2022-06-08 15:58:22 -0500

    function and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,17 +7,7 @@
 int print_last_digit(int c)
 {
 int last;
-if (c < 0)
-{
-c = (-1) * c;
-last = c % 10;
+last = abs(c % 10);
 _putchar(last + '0');
 return (last);
-}
-else
-{
-last = c % 10;
-_putchar(last + '0');
-return (last);
-}
 }
