e05c30adfe7c9c349a25d7bf337b1cce01b73bbc
TJchamp1 <tamratabrham9@gmail.com>
2022-06-08 17:14:27 -0500

    function and nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,26 +4,16 @@
  * Return - 0
  */
 int main() {
-
-  int i;
-
- 
-  int t1 = 1, t2 = 2;
-
- 
-  int nextTerm = t1 + t2;
-
- 
-  printf("Fibonacci Series: %d, %d, ", t1, t2);
-
-  
-  for (i = 4; i <= 50; ++i) 
-  {
-    printf("%d, ", nextTerm);
-    t1 = t2;
-    t2 = nextTerm;
-    nextTerm = t1 + t2;
-  }
-
-  return 0;
+int i;
+int t1 = 1, t2 = 2;
+int nextTerm = t1 + t2;
+printf("%d, %d, ", t1, t2);
+for (i = 4; i <= 50; ++i) 
+{
+printf("%d, ", nextTerm);
+t1 = t2;
+t2 = nextTerm;
+nextTerm = t1 + t2;
+}
+return (0);
 }
