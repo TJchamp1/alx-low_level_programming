545e71244250093a3b42a621cd7b487bd8a697a5
TJchamp1 <tamratabrham9@gmail.com>
2022-06-09 16:13:09 -0500

    function and nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_lines - prints lines
+ * Return : No return
+ */
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++ )
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
