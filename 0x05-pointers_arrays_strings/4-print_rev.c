bbc3cf7ed9e2eb75b70befc8b669a8d81d53887f
TJchamp1 <tamratabrham9@gmail.com>
2022-06-13 07:32:25 -0500

    function and nested loops

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,15 @@
 #include<string.h>
 /**
  * print_rev - function
+ * @s: will be used
  */
 void print_rev(char *s)
 {
 int len, i;
 len = strlen(s);
-for (i = len - 1; i >= 0; i--)
+for (i = len; i >= 0; i--)
 {
-printf("%c", *s);
+printf("%c", *(s + i));
 }
 printf("\n");
 }
