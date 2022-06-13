cb54e9e47cee59d90f381885b398282ce0a11aae
TJchamp1 <tamratabrham9@gmail.com>
2022-06-13 08:42:00 -0500

    function and nested loops

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ * print_array - function
+ * @a: will be used
+ * @n: will be used
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n - 1; i++)
+{
+printf("%d, ", a[i]);
+}
+printf("%d", a[n - 1]);
+}
