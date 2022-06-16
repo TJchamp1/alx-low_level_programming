2b031113f2e1eda9065ef7434a968a3851d3b582
TJchamp1 <tamratabrham9@gmail.com>
2022-06-16 15:29:14 -0500

    variables task 12

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<string.h>
+/**
+ * more_numbers - function
+ * Return: noting
+ */
+char *_strcat(char *dest, char *src)
+{
+int n, i, n2, j = 0;
+n = strlen(dest);
+n2 = strlen(src);
+dest[n] =  " ";
+for (i = n + 1; i <= n2; i++)
+{
+dest[i] = src[j];
+j++;
+}
+return (dest);
+}
