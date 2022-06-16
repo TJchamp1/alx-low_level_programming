d91a9d4fa23726871731a03c57a7e7291f7487c7
TJchamp1 <tamratabrham9@gmail.com>
2022-06-16 15:49:02 -0500

    variables task 12

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include<string.h>
 /**
- * *_strcat - function
- * @dest: will be used
- * @src: will be used
- * Return: noting
- */
+* *_strcat - function
+* @dest: will be used
+* @src: will be used
+* Return: noting
+*/
 char *_strcat(char *dest, char *src)
 {
 int n, i, n2, j = 0;
-n = strlen(dest);
+n =strlen(dest);
 n2 = strlen(src);
 dest[n] = ' ';
 for (i = n; i <= n2; i++)
