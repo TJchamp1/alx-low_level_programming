d59ae166bf512f99e047e467cf568f2776248e4a
TJchamp1 <tamratabrham9@gmail.com>
2022-06-16 16:00:41 -0500

    variables task 12

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,14 @@
 char *_strcat(char *dest, char *src)
 {
 int i = 0, j = 0;
-while (dest[i]!='\0')  
+while (dest[i] != '\0')  
 {
-++str1;  
+++src;  
 i++;  
 }  
 while (src[j] != '\0')  
 {  
-*dest=*src;  
+*dest = *src;  
 dest++;  
 src++;  
 j++;  
