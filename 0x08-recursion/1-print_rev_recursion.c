99a8f0909bfb7e203d66cf368e6e4cbbd2c26053
TJchamp1 <tamratabrham9@gmail.com>
2022-06-22 14:00:16 -0500

    variables task 12

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+i#include "main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ * _print_rev_recursion(char *s);- function
+ * @s: will be used
+ */
+void _print_rev_recursion(char *s)
+{
+printf("%s\n", strrev(s));
+}
